d5ca9ee5acc850885a9bdf74c0b99c79a065fd24
MarieMueller <m.mueller470718@gmail.com>
2022-11-10 21:49:27 -0600

    Day 3 mini project - Treasure Island game

---
+++ projects/treasure-island/README.md
@@ -0,0 +1,28 @@
+# treasure-island
+
+Program that user can choose their own adventure game. For this project, only conditional statements are used to tell the story and path of the game.
+
+<!-- Add images/gifs -->
+<!-- ![codename-generator](https://github.com/mariemueller-codes/python-mini-projects/blob/main/assets/xxx.gif) -->
+
+## Prerequisites
+
+Python installed on your local machine.
+[Link](https://www.python.org/downloads/) to install it
+
+## Credits
+* 100 Days of Code: The Complete Python Pro Bootcamp for 2022
+
+<!-- ## Usage
+```
+python helloWorld.py
+```
+
+The final result will be printed on the screen as:
+
+```
+Hello World!
+``` -->
+
+## License
+[MIT License](https://github.com/mariemueller-codes/python-mini-projects/LICENSE.txt)
--- projects/treasure-island/treasureIsland.py
+++ projects/treasure-island/treasureIsland.py
@@ -1,5 +1,27 @@
-# import image
+import image
 
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
+print("You're at the crossroads. Where do you want to go? ")
+choice = input('Type "left" or "right" ')
+
+if (choice == "left"):
+    print("You've come to a lake. There is an island in the middle of the lake.")
+    choice2 = input('Type "wait" to wait for a boat. Type "swim" to swim across.')
+    if(choice2 == "wait"):
+        print("You arrived at the island unharmed.")
+        print("There is a house with 3 doors. One red, one yellow and one blue.")
+        door_color = input("Which color do you choose? ")
+        if(door_color == "yellow"):
+            print("You win!")
+        elif(door_color == "red"):
+            print("Burned by fire. Game Over.")
+        elif(door_color == "blue"):
+            print("Eaten by beasts. Game Over.")
+        else:
+            print("Game Over")
+    else:
+        ("You get attacked by an angry trout. Game Over.")
+else:
+    print("Fall into a hole. Game Over.")

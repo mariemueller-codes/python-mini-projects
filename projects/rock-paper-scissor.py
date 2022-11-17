f6f61adb0fef2301bf6c7871234fc3a84001f84c
MarieMueller <m.mueller470718@gmail.com>
2022-11-17 11:20:14 -0600

    Add rock paper scissor mini project

--- projects/rock-paper-scissor.py
+++ projects/rock-paper-scissor.py
@@ -27,7 +27,40 @@ scissors = '''
 '''
 
 import random
-choice = ['rock', 'paper', 'scissors']
-# guess = input("Choose rock, paper or scissors: ").lower()
-computer_choice = random.choices(choice)
-print(computer_choice)
+choices = ['rock', 'paper', 'scissors']
+guess = input("Choose rock, paper or scissors: ").lower()
+computer_choice = choices[random.randint(0,2)]
+
+def print_image(item):
+    if item == 'rock':
+        print(rock)
+    elif item == 'paper':
+        print(paper)
+    elif item == 'scissors':
+        print(scissors)
+
+print(f"\nYou choose: {(guess)}")
+print_image(guess)
+print(f"\nComputer chooses: {(computer_choice)}")
+print_image(computer_choice)
+
+# game
+if guess == computer_choice:
+    print("It is a tie!")
+elif guess == 'rock':
+    if computer_choice == 'paper':
+        print("You lose")
+    else:
+        print("You win")
+elif guess == 'paper':
+    if computer_choice == 'scissors':
+        print("You lose")
+    else:
+        print("You win")
+elif guess == 'scissors':
+    if computer_choice == 'rock':
+        print("You lose")
+    else:
+        print("You win")
+else:
+    print("Not a valid play")
